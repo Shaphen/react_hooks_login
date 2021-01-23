182b4707083be47d8259d3473d8918fabb23adad
Shaphen <swpburn@gmail.com>
2021-01-23 12:39:16 -0800

    add submit logic to class component for login

--- src/components/session_class/login2.jsx
+++ src/components/session_class/login2.jsx
@@ -4,15 +4,52 @@ class Login extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-
+      username: "",
+      password: ""
     }
+  }
+
+  handleChange(type) {
+    return e => {
+      this.setState({ [type]: e.target.value })
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.login(this.state)
   }
 
   render(){
     return (
       <div className="login-container">
-        
+        <h1 className="login-title">Welcome Back!</h1>
+
+        <form className="login-form">
+          <input
+            type="text"
+            name="username"
+            className="login-input"
+            placeholder="Username"
+            value={ this.state.username }
+          />
+          <input
+            type="text"
+            name="password"
+            className="login-input"
+            placeholder="Password"
+            value={ this.state.password }
+          />
+
+
+        </form>
       </div>
     )
   }
 }
+
+// const mDTP = dispatch => ({
+//   login: user => dispatch(login(user))
+// });
+
+export default Login;
